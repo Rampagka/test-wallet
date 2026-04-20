d06828236268d711905482b4490763cfc61ba18a
Rampage <fedyaevandrey@yandex.ru>
2026-04-21 02:30:52 +0500

    fix(styles): fix placeholder opacity and navbar scroll padding

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ const showNavBar = computed(() => !route.meta.isPublic)
 <template>
     <v-app>
         <v-main>
-            <div class="app-container">
+            <div class="app-container" :class="{ 'app-container--with-nav': showNavBar }">
                 <router-view />
             </div>
         </v-main>
@@ -25,6 +25,11 @@ const showNavBar = computed(() => !route.meta.isPublic)
 .app-container {
     max-width: 428px;
     margin: 0 auto;
-    min-height: calc(100dvh - 70px);
+    min-height: 100dvh;
+}
+
+.app-container--with-nav {
+    min-height: calc(100dvh - 56px);
+    padding-bottom: 72px;
 }
 </style>
